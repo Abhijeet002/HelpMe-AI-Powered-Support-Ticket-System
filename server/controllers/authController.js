51a71aa3c4a68acdb1dbe17e27da913cb54d5ef1
Abhijeet <abhijeetsachan.088@gmail.com>
2025-06-28 03:09:24 +0530

    add basic auth controllers

---
+++ server/controllers/authController.js
@@ -0,0 +1,79 @@
+import { User } from "../models/User.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+dotenv.config();
+
+export const register = async (req, res) => {
+  const { username, email, password, role } = req.body;
+  try {
+    const existingUser = await User.findOne({
+      username: username,
+      email: email,
+    });
+    if (existingUser) {
+      res.status(400).json({ message: "User already exists" });
+      return;
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+      role,
+    });
+    await newUser.save();
+
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (error) {
+    console.error("Error registering user:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user || user.length === 0) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    const token = jwt.sign(
+      {
+        id: user._id,
+        role: user.role,
+        email: user.email, //  safe to include
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "7d" }
+    );
+    res
+      .cookie("token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "Strict",
+        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+      })
+      .status(200)
+      .json({ message: "Login successful", role: user.role, name: user.name });
+  } catch (error) {
+    console.error("Error logging in user:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+// controllers/authController.js
+export const logout = (req, res) => {
+  res
+    .clearCookie('token', {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'Strict'
+    })
+    .status(200)
+    .json({ message: 'Logged out successfully' });
+};
+
