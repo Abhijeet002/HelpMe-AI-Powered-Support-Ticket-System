51c188b006adb1be028ac78688635fbd882aa95c
Abhijeet <abhijeetsachan.088@gmail.com>
2025-06-29 00:36:39 +0530

    Define Ticket Schema

---
+++ server/models/Ticket.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+const ticketSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    priority: {
+      type: String,
+      enum: ["low", "medium", "high"],
+      default: "medium",
+    },
+    status: {
+      type: String,
+      enum: ["open", "in-progress", "closed"],
+      default: "open",
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+export const Ticket = mongoose.model("Ticket", ticketSchema);
