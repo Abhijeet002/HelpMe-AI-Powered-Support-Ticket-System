c99a50d1a216e126c9ce7af671cf04e9e9f0c36e
Abhijeet <abhijeetsachan.088@gmail.com>
2025-07-01 19:54:37 +0530

    add role middleware

--- server/index.js
+++ server/index.js
@@ -4,22 +4,25 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
 import authRoutes from './routes/authRoutes.js';
+import ticketRoutes from './routes/ticketRoutes.js';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 const app = express();
-
+app.use(cookieParser());
 app.use(cors());
 app.use(express.json());
 
 connectDB();
 
 app.use('/api/auth', authRoutes);
+app.use('/api/ticket', ticketRoutes);
 
 app.get('/', (req, res) => {
-  res.send('Welcome to the Authentication API');
+  res.send('Welcome to the Ticket API');
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
---
+++ server/middleware/roleMiddleware.js
@@ -0,0 +1,17 @@
+// middleware/roleMiddleware.js
+
+export const roleMiddleware = (allowedRoles = []) => {
+  return (req, res, next) => {
+    const userRole = req.user?.role;
+
+    if (!userRole) {
+      return res.status(401).json({ message: 'Unauthorized: No user role found' });
+    }
+
+    if (!allowedRoles.includes(userRole)) {
+      return res.status(403).json({ message: 'Forbidden: You do not have access to this resource' });
+    }
+
+    next();
+  };
+};
