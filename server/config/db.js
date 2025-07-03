0341d2e38a62b3b1e77c361649c4170997c3d6b4
Abhijeet <abhijeetsachan.088@gmail.com>
2025-07-03 20:16:07 +0530

    update db

--- server/config/db.js
+++ server/config/db.js
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();    
 
+// Connect to MongoDB
 const connectDB = async () => {
-    try{
+    try {
         await mongoose.connect(process.env.MONGO_URI);
         console.log("MongoDB connected successfully");
-    }
-    catch(error){
+    } catch (error) {
         console.error("MongoDB connection failed:", error.message);
         console.log("Please check your MongoDB URI and ensure the database is running.");
         console.error(error.stack);
