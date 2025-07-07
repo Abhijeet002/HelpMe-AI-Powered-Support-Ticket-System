3b0f9fd2f94378598f4297a6a24fd394d3996fe6
Abhijeet <abhijeetsachan.088@gmail.com>
2025-07-08 01:39:46 +0530

    update reply schema

--- server/models/Reply.js
+++ server/models/Reply.js
@@ -1,32 +1,32 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const replySchema = new mongoose.Schema(
   {
     ticket: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: 'Ticket',
-      required: true
+      ref: "Ticket",
+      required: true,
     },
     user: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: 'User',
-      required: true
+      ref: "User",
+      required: true,
     },
     message: {
       type: String,
-      required: true
+      required: true,
     },
     senderRole: {
       type: String,
-      enum: ['user', 'agent', 'admin'],
-      required: true
+      enum: ["user", "agent", "admin"],
+      required: true,
     },
     attachment: {
-      type: String, // Store the file name or cloud URL
-      default: null
-    }
+      url: { type: String },
+      public_id: { type: String },
+    },
   },
   { timestamps: true }
 );
 
-export const Reply = mongoose.model('Reply', replySchema);
+export const Reply = mongoose.model("Reply", replySchema);
