4e22fdb1fc59a58b60c8e7cafcd55e4cd5212eac
Abhijeet <abhijeetsachan.088@gmail.com>
2025-07-08 01:43:34 +0530

    update create replyroute

--- server/routes/replyRoutes.js
+++ server/routes/replyRoutes.js
@@ -1,17 +1,24 @@
 import express from "express";
-import { addReply, deleteReply, editReply, getRepliesForTicket } from "../controllers/replyController.js";
+import {
+  createReply,
+  deleteReply,
+  editReply,
+  getRepliesForTicket,
+} from "../controllers/replyController.js";
 import { verifyToken } from "../middleware/authMiddleware.js";
+import { upload } from "../middleware/upload.js";
+import { checkReplyAccess } from "../middleware/checkReplyAccess.js";
 
 const router = express.Router();
 
-// POST /api/replies/:ticketId
-router.post("/:ticketId", verifyToken, addReply);
-router.get('/:ticketId', verifyToken, getRepliesForTicket);
+// POST /api/replies/create/:ticketId
+router.post('/:ticketId', verifyToken, upload.single('attachment'), createReply);
+
+router.get("/:ticketId", verifyToken, getRepliesForTicket);
 // PATCH /api/replies/edit/:replyId
-router.patch('/edit/:replyId', verifyToken, editReply);
+router.patch("/edit/:replyId", verifyToken, editReply);
 
 // DELETE /api/replies/delete/:replyId
-router.delete('/delete/:replyId', verifyToken, deleteReply);
-
+router.delete("/delete/:replyId", verifyToken, deleteReply);
 
 export default router;
