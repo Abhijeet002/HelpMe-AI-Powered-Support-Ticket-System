2f6496d7d9b1ed84c585c17c48b7328980d3bd90
Abhijeet <abhijeetsachan.088@gmail.com>
2025-07-24 00:34:57 +0530

    client directory configuration

---
+++ client/src/app/layout.tsx
@@ -0,0 +1,21 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { ReactNode } from 'react'
+// import ReduxProvider from '@/providers/ReduxProvider'
+
+export const metadata: Metadata = {
+  title: 'HelpMe',
+  description: 'AI-Powered Support Ticket System',
+}
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body>
+        {/* <ReduxProvider> */}
+          {children}
+        {/* </ReduxProvider> */}
+      </body>
+    </html>
+  )
+}
